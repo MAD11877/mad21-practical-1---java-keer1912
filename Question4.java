0fcf6aedec30769a3c22bd804fdec89f2591931f
Keerthana Keshaini <S10194171@connect.np.edu.sg>
2021-04-24 00:19:14 +0800

    Done Question 4

--- Question4.java
+++ Question4.java
@@ -18,6 +18,15 @@ public class Question4
      */
      
     Scanner in = new Scanner(System.in);
-    
+    int input = in.nextInt();
+ 
+    for (int i = input-1; i>=0 ; i--)//i-- gives you the value of i (5) then decrements it by 1.
+    {
+      for (int j=0; j<=i; j++)
+      {
+        System.out.print("*");
+      }
+    System.out.println();
+    }
   }
 }
